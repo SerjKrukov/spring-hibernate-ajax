0d68a195bd32eb58caf7749543bc2d17ff5828d4
SerjKrukov <serj.krukov@gmail.com>
2017-04-27 08:47:22 +0300

    Update Application.java

--- src/main/java/test/Application.java
+++ src/main/java/test/Application.java
@@ -3,9 +3,6 @@ package test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by Serj on 09.04.2017.
- */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
